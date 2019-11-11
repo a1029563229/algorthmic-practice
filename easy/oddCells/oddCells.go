7a576ca45c203e1f1af87c9ea67fc3bbfa440eea
lt <1029563229@qq.com>
2019-11-11 09:25:20 +0800

    补充算法

---
+++ easy/oddCells/oddCells.go
@@ -0,0 +1,14 @@
+package main
+
+func oddCells(n int, m int, indices [][]int) int {
+	var N int
+
+	return N
+}
+
+func main() {
+	indices := [2][2]int{
+		{0, 1},
+		{1, 1}}
+	N := oddCells(2, 3, indices)
+}
